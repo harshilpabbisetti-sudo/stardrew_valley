49dd9419abc33990f6e871ed85e0482e67b300f6
harshil <harshil.pabbisetti@gmail.com>
2026-02-08 23:26:55 +0530

    animation

--- code/player.py
+++ code/player.py
@@ -30,26 +30,41 @@ class Player(pygame.sprite.Sprite):
         for animation in self.animations.keys():
             full_path = f'graphics/character/{animation}'
             self.animations[animation] = import_folder(full_path)
+
+    def animate(self, dt):
+        self.frame_index += 4 * dt
+        if self.frame_index >= len(self.animations[self.status]):
+            self.frame_index -= len(self.animations[self.status])
+        self.image = self.animations[self.status][int(self.frame_index)]
+
     def input(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_UP]:
             self.dirn.y = -1
+            self.status = 'up'
         elif keys[pygame.K_DOWN]:
             self.dirn.y = 1
+            self.status = 'down'
         else:
             self.dirn.y = 0
 
         if keys[pygame.K_RIGHT]:
             self.dirn.x = 1
+            self.status = 'right'
         elif keys[pygame.K_LEFT]:
             self.dirn.x = -1
+            self.status = 'left'
         else:
             self.dirn.x = 0
+
+    def _get_status(self, state):
+        if self.direction.magnitude() == 0:
+            self.status = self.status.split('_')[0] + '_idle'
     
     def move(self, dt):
         if self.dirn.magnitude() != 0:
-            self.dirn.normalize()
+            self.dirn = self.dirn.normalize()
 
         # horizontal movement
         self.pos.x += self.dirn.x * self.speed * dt
@@ -60,4 +75,6 @@ class Player(pygame.sprite.Sprite):
 
     def update(self, dt):
         self.input()
+        self._get_status(self.status)
         self.move(dt)
+        self.animate(dt)
