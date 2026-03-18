f1b99c712436664298c3cb69c29c1b6f5fa67804
harshil <harshil.pabbisetti@gmail.com>
2026-03-18 19:59:48 +0530

    trees

--- code/sprites.py
+++ code/sprites.py
@@ -1,5 +1,7 @@
 import pygame
 from settings import *
+from random import randint, choice
+from timer import Timer
 
 
 class Generic(pygame.sprite.Sprite):
@@ -25,7 +27,7 @@ class Water(Generic):
 
     def animate(self, dt):
         self.frame_index += 5 * dt
-        if self.frame_index >= len(self.frames):
+        while self.frame_index >= len(self.frames):
             self.frame_index -= len(self.frames)
         self.image = self.frames[int(self.frame_index)]
 
@@ -38,6 +40,50 @@ class WildFlower(Generic):
         super().__init__(pos, surf, groups)
         self.hitbox = self.rect.copy().inflate((-20, -self.rect.height*0.9))
 
+
 class Tree(Generic):
     def __init__(self, pos, surf, groups, name):
         super().__init__(pos, surf, groups)
+
+        # tree attributes
+        self.health = 5
+        self.alive = True
+        self.stump_surf = pygame.image.load(f'graphics/stumps/{name.lower()}.png').convert_alpha()
+        self.invul_timer = Timer(200)
+
+        # apples
+        self.apple_surf = pygame.image.load('graphics/fruit/apple.png').convert_alpha()
+        self.apple_pos = APPLE_POS[name]
+        self.apple_sprite = pygame.sprite.Group()
+        self.create_fruit()
+
+    def damage(self):
+        # damaging tree
+        self.health -= 1
+
+        # removing apple
+        if len(self.apple_sprite.sprites()) > 0:
+            random_apple = choice(self.apple_sprite.sprites())
+            random_apple.kill()
+
+
+    def create_fruit(self):
+        for pos in self.apple_pos:
+            if randint(0, 10) < 2:
+                x = pos[0] + self.rect.left
+                y = pos[1] + self.rect.top
+                Generic(pos=(x, y),
+                        surf=self.apple_surf,
+                        groups=[self.apple_sprite, self.groups()[0]],
+                        z=LAYERS['fruit'])     # index 0 is all_sprites
+
+    def check_death(self):
+        if self.health <= 0:
+            self.image = self.stump_surf
+            self.rect = self.image.get_rect(midbottom=self.rect.midbottom)
+            self.hitbox = self.rect.copy().inflate(-10, -self.rect.height *  0.6)
+            self.alive = False
+
+    def update(self, dt):
+        if self.alive:
+            self.check_death()
